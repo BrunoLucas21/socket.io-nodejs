9b4c19dfa926311c452506fff69ea63b95352c25
BrunoLucas21 <brunolucasafreire@gmail.com>
2023-12-13 08:52:05 -0300

    configuration docker

--- server.js
+++ server.js
@@ -1,18 +1,43 @@
 var express = require('express');
 var http = require('http');
 var socketIO = require('socket.io');
+// var mysql = require('mysql');
 
 var app = express();
 var server = http.createServer(app);
 var io = socketIO(server);
+
+// var db = mysql.createConnection({
+//     host: 'db',
+//     user: 'user',
+//     password: 'adm_dev25*',
+//     database: 'chatdb'
+// });
+
+// db.connect((err) => {
+//     if (err) {
+//         console.error('Database connection failed:', err.stack);
+//         return;
+//     }
+//     console.log('Connected to database');
+// });
 
 app.get('/', function(req, res) {
     res.sendFile(__dirname + "/index.html");
 });
 
 io.on('connection', function(socket) {
+    console.log('Usuário conectado!');
+
+    socket.on('disconnect', function() {
+        console.log('Usuário desconectado!');
+    });
+
     socket.on('chat message', (msg) => {
         io.emit('chat message', msg);
+
+        // Salvando a menssagem no banco de dados
+        // db.query('INSERT INTO messages (content) VALUES (?)')
     });
 });
 
